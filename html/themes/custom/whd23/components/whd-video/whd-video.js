37a6bf33240408156e390e61108f05aae5c6a3af
Chris Ruppel <ruppel@reliefweb.int>
2023-08-16 10:20:51 +0200

    fix: restrict YT related videos to UN channel

--- html/themes/custom/whd23/components/whd-video/whd-video.js
+++ html/themes/custom/whd23/components/whd-video/whd-video.js
@@ -42,7 +42,7 @@
 
   // Accepts a DOM container and replaces its contents with a YouTube iframe.
   function prepVideo(video) {
-    video.innerHTML = '<iframe class="video__iframe" src="https://www.youtube.com/embed/' + video.dataset.videoSlug + '?autoplay=1&playsinline=1&enablejsapi=1&origin=' + encodeURIComponent(video.dataset.videoOrigin) + '" frameborder="0" allow="autoplay; fullscreen" sandbox="allow-same-origin allow-scripts"></iframe>';
+    video.innerHTML = '<iframe class="video__iframe" src="https://www.youtube.com/embed/' + video.dataset.videoSlug + '?autoplay=1&playsinline=1&rel=0&enablejsapi=1&origin=' + encodeURIComponent(video.dataset.videoOrigin) + '" frameborder="0" allow="autoplay; fullscreen" sandbox="allow-same-origin allow-scripts"></iframe>';
   }
 
   // Warm TCP connections by adding <link>s to the <head>.
